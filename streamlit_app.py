b89b3cfedaaa3905196df84256bc4520850ea3f0
osborneazsf <131621919+osborneazsf@users.noreply.github.com>
2023-05-03 11:28:35 +0100

    SF function

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,15 +46,34 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 # don't run anything after this
-streamlit.stop()
+# streamlit.stop()
+
+streamlit.header("The fruit load list contains:")
+
+# snowflake related functions
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+## add a button to load the list
+
+if streamlit.button("Get Fruit Load List")
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 # import snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+
+#streamlit.dataframe(my_data_rows)
+
+
+streamlit.stop()
 
 #adding a single-line text input widget
 
