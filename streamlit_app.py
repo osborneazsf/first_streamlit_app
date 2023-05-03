592e6812308895ff9eef3de1f2eaecd234f72082
osborneazsf <131621919+osborneazsf@users.noreply.github.com>
2023-05-03 11:04:51 +0100

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 
  except URLError as e:
-    streamlist.error()
+    streamlit.error()
   
 streamlit.write('The user entered ', fruit_choice)
 
