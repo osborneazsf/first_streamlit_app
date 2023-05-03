d8f53f97f1228dad682c787ddd06c1a1c9b82583
osborneazsf <131621919+osborneazsf@users.noreply.github.com>
2023-05-03 11:36:36 +0100

    dynamic add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ if streamlit.button("Get Fruit Load List"):
 #adding a single-line text input widget
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list VALUES ('from streamlit')")
+    my_cur.execute("insert into fruit_load_list VALUES ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
